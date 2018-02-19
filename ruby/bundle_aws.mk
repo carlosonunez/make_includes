49798dee7544a16132da4fec590d384f0b498c5e
Carlos Nunez <dev@carlosnunez.me>
2018-02-19 16:12:17 -0600

    dev@carlosnunez.me  |  Fix bug with parsing bundle action.

--- ruby/bundle_aws.mk
+++ ruby/bundle_aws.mk
@@ -8,7 +8,7 @@ bundle_%: \
 	_verify_variable-AWS_REGION \
 	_verify_variable-AWS_ACCESS_KEY_ID \
 	_verify_variable-AWS_SECRET_ACCESS_KEY
-bundle_%: BUNDLE_ACTION=$(shell echo "$@" | cut -f3 -d )
+bundle_%: BUNDLE_ACTION=$(shell echo "$@" | cut -f3 -d ' ' | cut -f2 -d '_')
 bundle_%:
 	docker run --rm -t -v $$PWD:/work -w /work \
 		-v $$PWD/.gem:/root/.gem \
